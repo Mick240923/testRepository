54a30e8506d6175edb42810e49198fd319f9013b
unknown <mcolon2409@gmail.com>
2021-03-27 15:18:41 -0500

    first commit

---
+++ Question4.java
@@ -0,0 +1,32 @@
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double itemprice=10.99;
+System.out.println("This is the cost of what I want to buy:"+itemprice);
+double moneyinwallet=25.50;
+System.out.println("I have this amount of money in my wallet:"+moneyinwallet);
+double newamountinwallet= moneyinwallet-itemprice;
+System.out.println ("This is the new amount in wallet after purchase:"+newamountinwallet);
+int age = 48;
+System.out.println("My current age is:"+age);
+int numberoffriends = 200;
+System.out.println("I estimated my number of frineds:"+numberoffriends);
+double avgnumberoffriendsperyr = numberoffriends/age;
+System.out.println("This is the average number of friends I made per year:"+avgnumberoffriendsperyr);
+String firstname = "Michael";
+System.out.println("This is my real first name:"+firstname);
+char middleinitial = 'F';
+System.out.println("My middle initial:"+middleinitial);
+String lastname = "Colon";
+System.out.println("My last name:"+lastname);
+String myfullname = firstname+middleinitial+lastname;
+System.out.println("My full name is;"+myfullname);
+
+
+
+		
+	}
+
+}
